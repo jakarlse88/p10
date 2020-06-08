b7a4ea299e111b7494c49e23b7fa2929b23c7ee0
Jon Karlsen <karlsen.jonarild@gmail.com>
2020-06-08 16:00:03 +0200

    hotfix/note-title
    It should no longer be possible to submit the Note create/edit form without entering a Title

--- src/Client/Abarnathy.BlazorClient/Client/Shared/Components/NoteEditForm/NoteEditForm.razor.cs
+++ src/Client/Abarnathy.BlazorClient/Client/Shared/Components/NoteEditForm/NoteEditForm.razor.cs
@@ -1,14 +1,31 @@
 ﻿using Abarnathy.BlazorClient.Client.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.Web;
 
 namespace Abarnathy.BlazorClient.Client.Shared.Components.NoteEditForm
 {
     public partial class NoteEditForm
     {
+        [Parameter] public ComponentMode Mode { get; set; }
         [Parameter] public NoteInputModel InputModel { get; set; }
         [Parameter] public EventCallback<MouseEventArgs> SubmitCallback { get; set; }
         [Parameter] public EventCallback<MouseEventArgs> CancelCallback { get; set; }
         [Parameter] public APIOperationStatus OperationStatus { get; set; }
+        private bool _isValid;
+        private EditContext _editContext;
+
+        protected override void OnInitialized()
+        {
+            _editContext = new EditContext(InputModel);
+            
+            _editContext.OnFieldChanged += (sender, e) =>
+            {
+                _isValid = _editContext.Validate();
+                StateHasChanged();
+            };
+
+            _isValid = Mode != ComponentMode.Create && _editContext.Validate();
+        }
     }
 }
